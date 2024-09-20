18043ba43d9c8ad0ae60ac673111753c695b3fe0
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-09-20 13:26:25 +0000

    sadhjasdfds

--- PP1_4.py
+++ PP1_4.py
@@ -2,15 +2,27 @@
 
 def q1():
   #Write Assignment code here
+  words = input("Hello")
+  print(words)
 
 def q2():
   #Write Assignment code here
+  word = "Hello " + "Edward"
+  print(word)
 
 def q3():
   #Write Assignment code here
+  words1 =input("Input your first name: ")
+  words2 =input("Input you last name: ")
+  words3 = f"Hello {words1} and {words2}"
+  print("Your name is: "+ words1 + words2)
 
 def q4():
   #Write Assignment code here
+  student =input("Input a student's name: ")
+  student1 =input("Input another student's name: ")
+  print("Your students are: "  +student + student1)
+
 
 #Do not edit code below this comment
 
